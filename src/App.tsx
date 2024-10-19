36606cc6703158fa42c029aebef87e5b151a5747
Anokhi1994 <anokhias@gmail.com>
2024-10-19 11:09:01 +0200

    fix: removed unused import React

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 import { ui, uiConfig } from './firebaseConfig';  // Correct import of ui and uiConfig
 
