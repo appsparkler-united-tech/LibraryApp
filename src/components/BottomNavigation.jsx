5cafc11107a0e6447f44044f4826d5fd32547114
Anokhi1994 <anokhias@gmail.com>
2024-11-05 19:09:35 +0100

    fix: added actions to all the buttons

--- src/components/BottomNavigation.jsx
+++ src/components/BottomNavigation.jsx
@@ -3,14 +3,21 @@ import Button from './Button';
 import { GrFavorite, GrAddCircle, GrChat, GrUser } from 'react-icons/gr';
 import { IoIosSearch } from 'react-icons/io';
 
-const BottomNavigation = ({ handleClick }) => (
-  <div className="btm-nav bg-teal-500">
-    <Button icon={IoIosSearch} label="Search" isActive="true" onClick={handleClick} />
-    <Button icon={GrFavorite} label="Favourite" isActive="true" onClick={handleClick} />
-    <Button icon={GrAddCircle} label="Offer" isActive="true" />
-    <Button icon={GrChat} label="Message" isActive="true" onClick={handleClick} />
-    <Button icon={GrUser} label="Me" isActive="true" onClick={handleClick} />
-  </div>
-);
+const BottomNavigation = () => {
+  const handleClick = (action) => {
+    console.log(action);
+  };
+
+  return (
+    <div className="btm-nav">
+      <Button icon={IoIosSearch} label="Search" isActive={true} onClick={() => handleClick("search your next read")} />
+      <Button icon={GrFavorite} label="Favourite" isActive={true} onClick={() => handleClick("mark your favourite")} />
+      <Button icon={GrAddCircle} label="Offer" isActive={true} onClick={() => handleClick("offer books")} />
+      <Button icon={GrChat} label="Message" isActive={true} onClick={() => handleClick("lets chat")} />
+      <Button icon={GrUser} label="Me" isActive={true} onClick={() => handleClick("my profile")} />
+    </div>
+  );
+};
 
 export default BottomNavigation;
+
