79fef593cecefc34dbb052be9647f92aef0ae4f1
Anokhi1994 <anokhias@gmail.com>
2024-11-05 15:09:46 +0100

    fix: bottom navigation is done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,13 @@
-// Button.js
+// Button.jsx
 import React from 'react';
 
-const Button = ({ icon, label, onClick, className }) => (
-  <button onClick={onClick} className={`flex flex-col items-center ${className}`}>
-    {icon}
-    <span className="btm-nav-label">{label}</span>
+const Button = ({ icon, label, isActive, onClick }) => (
+  <button
+    className={`flex flex-col items-center ${isActive ? 'bg-base-100' : 'bg-transparent'}`}
+    onClick={onClick}
+  >
+    <span className="h-5 w-5 text-black">{icon}</span> {/* Set icon color to black */}
+    <span className="btm-nav-label text-black">{label}</span> {/* Set label color to black */}
   </button>
 );
 
--- src/components/Button.stories.jsx
+++ src/components/Button.stories.jsx
@@ -1,35 +1,24 @@
 import React from 'react';
-import Button from './Button'; // Adjust the import path if needed
-
-const HomeIcon = (
-  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-  </svg>
-);
+import Button from './Button';
+import { GrFormSearch } from 'react-icons/gr';
 
 export default {
   title: 'Components/Button',
   component: Button,
-  argTypes: {
-    label: { control: 'text' },
-    isActive: { control: 'boolean' },
-    onClick: { action: 'clicked' },
-    icon: { control: false }, // Disable icon control if using a specific icon in stories
-  },
 };
 
 const Template = (args) => <Button {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  label: 'Favourite',
+  icon: GrFormSearch,
+  label: 'Search',
   isActive: false,
-  icon: <svg width="20" height="20" fill="black"><circle cx="10" cy="10" r="10" /></svg>, // Simple circle SVG
 };
 
 export const Active = Template.bind({});
 Active.args = {
-  label: 'Favourite',
+  icon: GrFormSearch,
+  label: 'Search',
   isActive: true,
-  icon: <svg width="20" height="20" fill="black"><circle cx="10" cy="10" r="10" /></svg>, // Simple circle SVG
 };
