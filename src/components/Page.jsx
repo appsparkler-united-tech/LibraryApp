5f5cf34419b754bc9942fe89bd905176686205c3
Anokhi1994 <anokhias@gmail.com>
2024-11-08 14:15:30 +0100

    feat: made the page static, without scrolling option

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -4,16 +4,15 @@ import BottomNavigation from './BottomNavigationComponent/BottomNavigation';
 
 const Page = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-between">
+    <div className="container mx-auto overflow-hidden">
       {/* Header Component */}
       <Header />
-      
+
       {/* Page Content */}
-      <main className="flex-grow p-4">
-        {/* Any page-specific content can go here */}
+      <main className="flex-grow p-4 overflow-hidden">
         <p className="text-center">Welcome to your page!</p>
       </main>
-      
+
       {/* Bottom Navigation Component */}
       <BottomNavigation />
     </div>
