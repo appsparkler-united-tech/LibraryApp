8a8f6e92bb9932681bf41f9ac98047747bd76757
Anokhi1994 <anokhias@gmail.com>
2024-11-08 14:25:59 +0100

    feat: added component folders

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './Header';
-import BottomNavigation from './BottomNavigationComponent/BottomNavigation';
+import Header from '../Header/Header';
+import BottomNavigation from '../BottomNavigationComponent/BottomNavigation';
 
 const Page = () => {
   return (
