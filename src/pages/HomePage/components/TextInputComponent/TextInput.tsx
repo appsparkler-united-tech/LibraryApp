8ee99e22ef330bc1965d33f3e7c6fb5b8d3782f4
Anokhi1994 <anokhias@gmail.com>
2024-11-10 16:12:20 +0100

    feat: daisyUI text input with search icon

--- src/pages/HomePage/components/TextInputComponent/TextInput.tsx
+++ src/pages/HomePage/components/TextInputComponent/TextInput.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 
 interface TextInputProps {
-  placeholder?: string;
+  placeholder: string;
 }
 
 const TextInput: React.FC<TextInputProps> = ({ placeholder }) => {
   return (
-    <input
-      type="text"
-      placeholder={placeholder}
-      className="input input-bordered w-full md:w-auto"
-    />
+    <div className="form-control">
+      <div className="relative">
+        <input
+          type="text"
+          className="input input-bordered w-full pr-12"
+          placeholder={placeholder}
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          className="h-5 w-5 absolute top-1/2 transform -translate-y-1/2 right-3 opacity-70"
+        >
+          <path
+            fillRule="evenodd"
+            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </div>
+    </div>
   );
 };
 
