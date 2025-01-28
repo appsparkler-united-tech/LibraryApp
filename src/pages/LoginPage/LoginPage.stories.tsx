b8721d03c258a4af45c7f60d55dbc4390dfdc875
Anokhi1994 <anokhias@gmail.com>
2025-01-28 16:24:43 +0100

    fix: removed routing to different page

--- src/pages/LoginPage/LoginPage.stories.tsx
+++ src/pages/LoginPage/LoginPage.stories.tsx
@@ -1,17 +1,8 @@
-import { MemoryRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./LoginPage";
-import MePage from "../ProfilePage/MePage";
 
 export default {
   title: "Pages/LoginPage",
   component: LoginPage,
 };
 
-export const Default = () => (
-  <MemoryRouter initialEntries={["/login"]}>
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/me" element={<MePage />} />
-    </Routes>
-  </MemoryRouter>
-);
+export const Default = () => <LoginPage />;
