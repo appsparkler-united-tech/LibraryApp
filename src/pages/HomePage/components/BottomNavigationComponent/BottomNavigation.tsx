de233f31eaa897ade76df980885c15e377a66a33
Anokhi1994 <anokhias@gmail.com>
2025-01-30 15:22:46 +0100

    feat: bottom nav updated to footer

--- src/pages/HomePage/components/BottomNavigationComponent/BottomNavigation.tsx
+++ src/pages/HomePage/components/BottomNavigationComponent/BottomNavigation.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { CiSearch, CiHeart, CiCirclePlus, CiChat1, CiUser} from "react-icons/ci";
-
+import { CiSearch, CiHeart, CiCirclePlus, CiChat1, CiUser } from "react-icons/ci";
 
 type BottomNavigationProps = {
   onClick: (action: string) => void;
@@ -8,32 +7,24 @@ type BottomNavigationProps = {
 
 const BottomNavigation: React.FC<BottomNavigationProps> = ({ onClick }) => {
   return (
-    <footer className="footer footer-horizontal bg-base-300 p-4">
+    <footer className="footer footer-horizontal footer-center rounded bg-base-300 p-4">
       <nav className="w-full flex justify-around">
         <a className="flex flex-col items-center cursor-pointer" onClick={() => onClick("search")}>
           <CiSearch className="text-2xl text-secondary-content" />
           <span className="text-xs text-secondary-content">Search</span>
         </a>
-        </nav>
-        <nav className="w-full flex justify-around">
         <a className="flex flex-col items-center cursor-pointer" onClick={() => onClick("favourite")}>
           <CiHeart className="text-2xl text-black" />
-          <span className="text-xs text-secondary-content">Favourite</span>
+          <span className="text-xs text-black">Favourite</span>
         </a>
-        </nav>
-        <nav className="w-full flex justify-around">
         <a className="flex flex-col items-center cursor-pointer" onClick={() => onClick("offer")}>
           <CiCirclePlus className="text-2xl text-black" />
           <span className="text-xs text-black">Offer</span>
         </a>
-        </nav>
-        <nav className="w-full flex justify-around">
         <a className="flex flex-col items-center cursor-pointer" onClick={() => onClick("message")}>
           <CiChat1 className="text-2xl text-black" />
           <span className="text-xs text-black">Message</span>
         </a>
-        </nav>
-        <nav className="w-full flex justify-around">
         <a className="flex flex-col items-center cursor-pointer" onClick={() => onClick("me")}>
           <CiUser className="text-2xl text-black" />
           <span className="text-xs text-black">Me</span>
